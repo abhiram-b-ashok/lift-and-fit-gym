9485fc11e407cb09b4f97e6e9fc6ed7a75ef4aa9
Abhiram B <abhiram.b.ashok@gmail.com>
2024-05-07 17:22:26 +0530

    blank link in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
     <footer className="footer">
       <div className="social-icons">
         <a href="tel:1234567890"><i className="fas fa-phone"></i></a>
-        <a href="https://www.youtube.com"><i className="fab fa-youtube"></i></a>
-        <a href="https://www.instagram.com/"><i className="fa-brands fa-square-instagram"></i></a>
+        <a href="https://www.youtube.com" target="_blank"><i className="fab fa-youtube"></i></a>
+        <a href="https://www.instagram.com/" target="_blank"><i className="fa-brands fa-square-instagram"></i></a>
       </div>
       <div className="copyright">
         &copy; 2024 Abhiram B
